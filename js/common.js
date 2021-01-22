89e06a5998834400b3fae91d0285dba6f2add3a1
Aleksandr <ascwork86@gmail.com>
2021-01-22 14:05:28 +0500

    add validation

--- js/common.js
+++ js/common.js
@@ -4,7 +4,13 @@ const
   textarea = document.querySelector('textarea'),
   selectLabel = document.querySelector('.custom-select-label'),
   selectDrop = document.querySelector('.custom-select-drop'),
-  selectValue = document.querySelector('.custom-select-value');
+  selectValue = document.querySelector('.custom-select-value'),
+  inputCity = document.querySelector('input[name="city"]'),
+  inputName = document.querySelector('input[name="name"]'),
+  inputPhone = document.querySelector('input[name="phone"]'),
+  btn = document.querySelector('.send__btn button'), 
+  formInputs = document.querySelectorAll('form input');
+
   
 
 textarea.addEventListener('keyup', function(){
@@ -25,7 +31,6 @@ function showSliderValue() {
   rangeBullet.style.left = (bulletPosition * 578) + "px";
 }
 
-
 document.querySelector('input[type=range]').addEventListener('input', function() {
   this.setAttribute('value', this.value);
 });
@@ -34,6 +39,29 @@ let dropSelect = () => {
   selectDrop.classList.add('open');  
   selectValue.classList.add('active'); // класс для стрелки
 }
+
+let validateForm = () => { 
+  let validate = false; 
+  if(inputCity.value != '' && inputName.value != '' && inputPhone.value != '') {
+    validate = true;
+    btn.classList.add('btn_active');
+  }else{
+    validate = false; 
+    btn.classList.remove('btn_active');
+  }
+
+  if(validate) btn.removeAttribute('disabled')
+  else btn.setAttribute('disabled', true)
+}
+
+formInputs.forEach( (elem, i) => {
+  elem.addEventListener('change', () => {
+    validateForm();
+  });
+  elem.addEventListener('input', () => {
+    validateForm();
+  });
+});
 
 selectLabel.addEventListener('click', (e) => {
   const elem = e.target;
@@ -44,6 +72,9 @@ selectLabel.addEventListener('click', (e) => {
 });
 
 selectValue.addEventListener('click', () => {  
+  if(selectValue.textContent == '' || !selectLabel.classList.contains('top')) {
+    selectLabel.classList.add('top');
+  }
   dropSelect();
 });
 
@@ -52,7 +83,8 @@ document.addEventListener('click', (e) => {
   if(elem.classList.contains('custom-select-drop__option')) {
     let val = elem.textContent;   
     selectValue.textContent = val;  
-    // тут надо добавить value в input type hidden    
+    inputCity.value = val;
+    validateForm();
   }
 });
 
@@ -61,57 +93,27 @@ document.addEventListener('click', (e) => {
   if(!elem.classList.contains('custom-select-label') && !elem.classList.contains('custom-select-drop') && !elem.classList.contains('custom-select-value')) {
     if(selectValue.textContent == '') {
       selectLabel.classList.remove('top');
-      console.log(1)
     }
     selectDrop.classList.remove('open');
     selectValue.classList.remove('active'); // класс для стрелки
   }
 });
 
-//   const overlay = document.querySelector('.overlay'),   
-// modal = document.querySelector('.modal');
-
-// document.addEventListener('click', (event) => {
-//   const element = event.target;
-//   if(element.classList.contains('btn')) {
-//       modal.classList.add('open');
-//       overlay.classList.add('open');
-//   }else if(element.classList.contains('cross')) {
-//       modal.classList.remove('open');
-//       overlay.classList.remove('open');
-//   }else{
-//       if(!element.closest('.modal')) {
-//           modal.classList.remove('open');
-//           overlay.classList.remove('open');
-//       }
-//   }
-// });
 
 const popup = document.querySelector('.form');
 const popup2 = document.querySelector('.form2');
 
-
 document.addEventListener('click', (event) => {
   const element = event.target;
-
   if(element.classList.contains('btn')) {
-
     popup2.classList.add('open');
-
     popup.classList.add('close');
-
   }else if(element.classList.contains('cross')){
-
     popup2.classList.remove('open');
-
     popup.classList.remove('open');
-
     popup.classList.add('close');
-
   }
 });
-
-
 
 // inputMask
 let inputs = document.querySelectorAll('.js-input-tel');
